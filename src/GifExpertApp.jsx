1f6eaf686be462cfdf418bcd730a93c5421a3817
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-07-21 22:13:09 -0400

    class 76

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
 
     const onAddCategory = () => {
-      setCategories([...categories, 'Attack on Titan']);
+      setCategories(['Attack on Titan', ...categories]);
     }
 
     return (
         <>
           <h1>GifExpertApp</h1>
+          <AddCategory/>
           <button onClick={onAddCategory}  >Agregar</button>
           <ol>
             { categories.map( category => {
