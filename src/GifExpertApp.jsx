7a07b879093c6e6c9872f85f2aaf1423f6498c39
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-04 17:10:21 -0400

    class 80

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,28 +1,32 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-    const onAddCategory = (newCategory) => {
-      if ( categories.includes(newCategory) ) return;
-      setCategories([newCategory, ...categories]);
-    }
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  }
 
-    return (
-        <>
-          <h1>GifExpertApp</h1>
-          <AddCategory 
-            // setCategories={ setCategories } 
-            onNewCategory={ event => onAddCategory(event) }
-          />
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory
+        // setCategories={ setCategories } 
+        onNewCategory={event => onAddCategory(event)}
+      />
 
-          <ol>
-            { categories.map( category => {
-              return <li key={ category }>{ category }</li>
-            })}
-          </ol>
-        </>
-    )
+
+      {
+        categories.map(category => (
+          <GifGrid key={category}
+            category={category} />
+        ))
+      }
+
+    </>
+  )
 }
