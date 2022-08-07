0e8d1e4190bf87f58c72ff25441f4640b417fa7f
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-06 21:08:54 -0400

    class 87

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -3,12 +3,15 @@ import { GifItem } from './Gifitem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-
-  const { images, isLoading} = useFetchGifs(category);
+  console.log(category)
+  const { images, isLoading} = useFetchGifs({category});
 
   return (
     <>
       <h3>{category}</h3>
+      {
+        isLoading && ( <h2>Cargando...</h2> )
+      }
 
       <div className="card-grid">
         {
