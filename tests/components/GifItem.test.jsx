b524dd9e904782015269529746cad456f80609e8
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-08 22:40:37 -0400

    class 102

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,6 +1,6 @@
 
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components/GifItem';
 
 describe('Pruebas en <GifItem />', () => { 
@@ -12,4 +12,22 @@ describe('Pruebas en <GifItem />', () => {
         const { container } = render ( <GifItem title={title} url={url}/> );
         expect(container).toMatchSnapshot();
      })
+
+     test('debe mostrar la imagen con  el IRL y el ALT indicado', () => { 
+
+        render ( <GifItem title={title} url={url}/> );
+        
+        //expect( screen.getByRole('img').src).toBe(url);
+        //expect( screen.getByRole('img').alt).toBe(title);
+        const {src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+      })
+
+      test('debe de mostrar el titulo en el componente', () => { 
+              
+            render ( <GifItem title={title} url={url}/> );
+            expect( screen.getByText(title) ).toBeTruthy();
+            
+       })
  })
