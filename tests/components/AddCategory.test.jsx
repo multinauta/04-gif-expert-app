1c48ca493d348d7953c51d788af49406a15f839b
alvaro tapia <alvarotapia@MacBook-Pro-de-alvaro.local>
2022-08-09 08:32:30 -0400

    class 106

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -13,8 +13,9 @@ describe('Pruebas en <AddCategory />', () => {
 
      test('debe de llamar onNewCatgory si el input tiene un valor', () => { 
          const inputValue = 'Saitama';
+         const onNewCategory = jest.fn();
 
-         render( <AddCategory onNewCategory={ () => {} }/> );
+         render( <AddCategory onNewCategory={ onNewCategory }/> );
 
          const input = screen.getByRole('textbox');
          const form = screen.getByRole('form');
@@ -22,5 +23,20 @@ describe('Pruebas en <AddCategory />', () => {
          fireEvent.input( input, { target: { value: inputValue } });
          fireEvent.submit( form );
          expect( input.value ).toBe('');
+
+         expect( onNewCategory ).toHaveBeenCalled();
+         expect( onNewCategory ).toHaveBeenCalledTimes(1);
+         expect( onNewCategory ).toHaveBeenCalledWith(inputValue);
       })
+
+      test('no debe de llamar el onNewCategory si el input está vació', () => { 
+       
+        const onNewCategory = jest.fn();
+        render( <AddCategory onNewCategory={ onNewCategory }/> );
+
+        const form = screen.getByRole('form');
+        fireEvent.submit( form );
+        
+        expect( onNewCategory ).not.toHaveBeenCalled();
+       })
  })
